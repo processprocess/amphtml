9af6e76ba0d8864c41c1fbaf16beef49dc42abfd
processprocess <philip.hunter.bell@gmail.com>
2021-02-09 09:59:10 -0500

    Update demo

--- extensions/amp-story-panning-media/0.1/amp-story-panning-media.js
+++ extensions/amp-story-panning-media/0.1/amp-story-panning-media.js
@@ -16,6 +16,7 @@
 
 import {
   Action,
+  UIType,
   StateProperty,
 } from '../../../extensions/amp-story/1.0/amp-story-store-service';
 import {CSS} from '../../../build/amp-story-panning-media-0.1.css';
@@ -77,14 +78,18 @@ export class AmpStoryPanningMedia extends AMP.BaseElement {
 
     /** @private {?string} */
     this.groupId_ = null;
+
+    /** @private {boolean} */
+    this.isDesktopPanelsMode_ = false;
   }
 
   /** @override */
   buildCallback() {
+    const zoomVal = parseFloat(this.element_.getAttribute('zoom'));
     this.animateTo_ = {
       x: parseFloat(this.element_.getAttribute('x')) || 0,
       y: parseFloat(this.element_.getAttribute('y')) || 0,
-      zoom: parseFloat(this.element_.getAttribute('zoom')) || 1,
+      zoom: zoomVal === 0 ? 0.000001 : zoomVal || 1,
     };
     return Services.storyStoreServiceForOrNull(this.win).then(
       (storeService) => {
@@ -115,6 +120,11 @@ export class AmpStoryPanningMedia extends AMP.BaseElement {
           left: 'auto',
           right: 'auto',
         });
+        this.animationState_ = this.animateTo_;
+        this.updateTransform_();
+        if (this.isOnActivePage_) {
+          this.animate_();
+        }
       })
       .catch(() => user().error(TAG, 'Failed to load the amp-img.'));
   }
@@ -132,6 +142,11 @@ export class AmpStoryPanningMedia extends AMP.BaseElement {
     this.storeService_.subscribe(
       StateProperty.PANNING_MEDIA_STATE,
       (panningMediaState) => this.onPanningMediaStateChange_(panningMediaState),
+      true /** callToInitialize */
+    );
+    this.storeService_.subscribe(
+      StateProperty.UI_STATE,
+      (uiState) => this.onUIStateUpdate_(uiState),
       true /** callToInitialize */
     );
     // Mutation observer for distance attribute
@@ -192,6 +207,10 @@ export class AmpStoryPanningMedia extends AMP.BaseElement {
         // Only call loop again if on active page.
         if (this.isOnActivePage_) {
           requestAnimationFrame(nextFrame);
+        } else {
+          this.storeService_.dispatch(Action.ADD_PANNING_MEDIA_STATE, {
+            [this.groupId_]: this.animateTo_,
+          });
         }
       } else {
         this.storeService_.dispatch(Action.ADD_PANNING_MEDIA_STATE, {
@@ -208,7 +227,7 @@ export class AmpStoryPanningMedia extends AMP.BaseElement {
    */
   onPanningMediaStateChange_(panningMediaState) {
     if (
-      this.distance_ <= 1 &&
+      this.distance_ <= (this.isDesktopPanelsMode_ ? 0 : 1) &&
       panningMediaState[this.groupId_] &&
       // Prevent update if value is same as previous value.
       // This happens when 2 or more components are on the same page.
@@ -261,6 +280,15 @@ export class AmpStoryPanningMedia extends AMP.BaseElement {
    */
   prefersReducedMotion_() {
     return this.win.matchMedia('(prefers-reduced-motion: reduce)').matches;
+  }
+
+  /**
+   * Reacts to UI state updates and triggers the expected UI.
+   * @param {!UIType} uiState
+   * @private
+   */
+  onUIStateUpdate_(uiState) {
+    this.isDesktopPanelsMode_ = uiState === UIType.DESKTOP_PANELS;
   }
 
   /** @override */
